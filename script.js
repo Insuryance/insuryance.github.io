081f223a090da4134c6bd947ff6c3432e7332f12
Insuryance <isitsuryansham@gmail.com>
2025-04-02 13:07:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector(".stay").style.transition =
                 "transform 1s ease-in-out";
             document.querySelector(".stay").style.transform =
-                "translateX(-150px)";
+                "translateX(-90px)";
 
             // Ensure smooth transition for final text
             setTimeout(() => {
